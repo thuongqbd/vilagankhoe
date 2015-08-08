6c2a74ee445ba35b21dd7288dc2e5d8705148fb9
thuongqbd <thuongqbd@gmail.com>
2015-08-08 14:05:25 +0700

    fix home page

--- wp-content/themes/vilagankhoe/page-templates/homepage.php
+++ wp-content/themes/vilagankhoe/page-templates/homepage.php
@@ -13,25 +13,26 @@
  * @subpackage Twenty_Fifteen
  * @since Twenty Fifteen 1.0
  */
-
-get_header(); ?>
+get_header();
+?>
 
 <div id="main-content" class="main-content">
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content container" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-					the_content();
-					// Include the page content template.
-//					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-//					if ( comments_open() || get_comments_number() ) {
-//						comments_template();
-//					}
-				endwhile;
-			?>
+			<div id="huongung" class="row">
+				<div id="video-lagan" class="left"><img src="http://localhost/vilagankhoe/wp-content/uploads/2015/08/video.png" alt="" /></div>
+				<div id="toihuongung" class="left">
+					<div class="top">
+						<button value="" id="btn-toihuongung"></button>
+					</div>
+					<div id="sohuongung">
+						<strong class="number"><?= number_format_i18n( get_post_meta( get_option('page_on_front'), 'concurred_count', true ), 0 );?></strong>
+						<span>người đã hưởng ứng</span>
+					</div>
+				</div>
+				
+				<div class="next right"></div>
+			</div>
 		</div><!-- #content -->
 	</div><!-- #primary -->
 </div><!-- #main-content -->
