8d4052dbd4d12b15c07b272718965606aec43bfe
rockchin <hoang_duyen2367@yahoo.com.vn>
2015-08-08 14:41:25 +0700

    duyen commit category tin tức, single tin tức

--- wp-content/themes/vilagankhoe/single.php
+++ wp-content/themes/vilagankhoe/single.php
@@ -24,19 +24,19 @@ get_header(); ?>
 			get_template_part( 'content', get_post_format() );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+//			if ( comments_open() || get_comments_number() ) :
+//				comments_template();
+//			endif;
 
 			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
+//			the_post_navigation( array(
+//				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
+//					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
+//					'<span class="post-title">%title</span>',
+//				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
+//					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
+//					'<span class="post-title">%title</span>',
+//			) );
 
 		// End the loop.
 		endwhile;
