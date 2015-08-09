bd1c5ee2a4a531cae1e78c7a8a4beb75d7df2f02
rockchin <hoang_duyen2367@yahoo.com.vn>
2015-08-09 17:02:26 +0700

    Duyen commit html, js popup

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,3 +1,53 @@
-jQuery(document).ready(function(){
-    jQuery('.scrollbar-inner').scrollbar();
+//jQuery(document).ready(function() {
+//	jQuery('.scrollbar-inner').scrollbar();
+//});
+$(function() {
+	if ($("#camon-fb").length && $('#huongung').length) {
+		var dialog = $("#camon-fb").dialog({
+			autoOpen: false,
+			height: 400,
+			width: 960,
+			modal: true
+		});
+
+		$("#btn-toihuongung").button().on("click", function() {
+			dialog.dialog("open");
+		});
+		$('.btn-close').click(function() {
+			dialog.dialog("close");
+		});
+	}
+	if ($("#dangky-test").length && $("#camon-dk").length) {
+		var dialog2 = $("#camon-dk").dialog({
+			autoOpen: false,
+			width: 316,
+			height: 290,			
+			modal: true
+		});
+
+		$("#btn-dangky-test").on('click',function(event) {			
+			event.preventDefault();			
+			dialog2.dialog("open");
+		});
+		$('.btn-close').click(function() {			
+			dialog2.dialog("close");
+		});
+	}
+	if ($("#datcauhoi").length && $("#camon-dch").length) {
+		var dialog3 = $("#camon-dch").dialog({
+			autoOpen: false,
+			width: 316,
+			height: 290,			
+			modal: true
+		});
+
+		$("#btn-guicauhoi").on('click',function(event) {			
+			event.preventDefault();			
+			dialog3.dialog("open");
+		});
+		$('.btn-close').click(function() {			
+			dialog3.dialog("close");
+		});
+	}
+
 });
